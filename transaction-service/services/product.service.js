074d059e7be5c8d309ca8591223d658a5fe18882
Kadek Guntur <kadekguntur13@gmail.com>
2025-08-02 11:11:48 +0800

    fix transactions user & product service

--- transaction-service/services/product.service.js
+++ transaction-service/services/product.service.js
@@ -1,7 +1,10 @@
 const axios = require('axios');
+const { Product } = require('../models');
 require('dotenv').config();
 
 exports.getProductById = async (id) => {
-  const response = await axios.get(`${process.env.PRODUCT_SERVICE_URL}/${id}`);
-  return response.data;
+  // const response = await axios.get(`${process.env.PRODUCT_SERVICE_URL}/${id}`);
+  // return response.data;
+
+  return await Product.findByPk(id);
 };
